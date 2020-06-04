d62047513459920f165eae25f5ceb517e49df7ee
huydinh3010 <huydinh3010@gmail.com>
2020-06-05 00:04:31 +0700

    tuan 12

--- sources/ArduinoCode/esp8266_src/esp8266_src.ino
+++ sources/ArduinoCode/esp8266_src/esp8266_src.ino
@@ -4,24 +4,30 @@
 #include <NTPClient.h>
 #include <Ticker.h>
 #include <WiFiUdp.h>
+#include <ESP8266WebServer.h>
+#include <WiFiClient.h>
+#include <EEPROM.h>
 
 
 #define FIREBASE_HOST "irremote-4f614.firebaseio.com"
 #define FIREBASE_AUTH "KPv43AaJnuulZW8YvMNwKIZkvZj9wOie365KjzPg"
-#define WIFI_SSID "ASUS_X00TD"
-#define WIFI_PASSWORD "huydinh3010"
+
 
 #define MAXLEN 500
 #define RXPINIR 5
 #define IRLEDPIN 13
 #define LEDPIN 15
-#define WFLED 0
+#define WFLED 4
+#define FLASHPIN 0
 
+String WIFI_SSID = "";
+String WIFI_PASSWORD = "";
 
 bool sendIR(String);
 
 WiFiUDP ntpUDP;
 NTPClient timeClient(ntpUDP, "asia.pool.ntp.org", 0, 3600000L);
+ESP8266WebServer server(80);
 
 volatile unsigned int last = 0;
 volatile unsigned int irBuffer[MAXLEN];
@@ -30,6 +36,8 @@ volatile int val = 0;
 String data = "";
 uint16_t rawLen = 0;
 byte mode = 0;
+bool hasWifiInfo = false;
+bool flashPressed = false;
 
 class SenderSchedule{
   public:
@@ -68,6 +76,10 @@ class SenderSchedule{
       st = true;
     };
 } schedule[10];
+
+void ICACHE_RAM_ATTR flash_Interrupt_Handler() { // hàm xử lý ngắt
+  flashPressed = true;
+}
 
 void ICACHE_RAM_ATTR rxIR_Interrupt_Handler() { // hàm xử lý ngắt
   val = digitalRead(RXPINIR);
@@ -288,60 +300,166 @@ void doCmd(String cmd){
   }
 }
 
+bool lockupWifiInfo(){
+  EEPROM.begin(512);
+  int w_a = EEPROM.read(0);
+  if(w_a){
+    int i = 1;
+    int _b = -1;
+    WIFI_SSID = "";
+    WIFI_PASSWORD = "";
+    while(_b != 0 && i < 512){
+      _b = EEPROM.read(i++);
+      if(_b != 0) WIFI_SSID += (char)_b;
+    }
+    _b = -1;
+    while(_b != 0 && i < 512){
+      _b = EEPROM.read(i++);
+      if(_b != 0) WIFI_PASSWORD += (char)_b;
+    }
+    EEPROM.end();
+    return true;
+  } else{
+    return false;
+  }
+}
+
+void writeWifiInfoToEEPROM(String wf_name, String wf_pass){
+  EEPROM.begin(512);
+  EEPROM.write(0, 1);
+  delay(5);
+  int e_in = 1;
+  for(int i = 0; i < wf_name.length(); i++){
+    EEPROM.write(e_in++, (int)wf_name[i]);
+    delay(5);
+  }
+  EEPROM.write(e_in++, 0);
+  delay(5);
+  for(int i = 0; i < wf_pass.length(); i++){
+    EEPROM.write(e_in++, (int)wf_pass[i]);
+    delay(5);
+  }
+  EEPROM.write(e_in++, 0);
+  delay(100);
+  EEPROM.commit();
+  EEPROM.end();
+}
+
+void handleWSRoot(){
+  if(server.hasArg("name") && server.hasArg("pass")){
+    String wf_name = server.arg("name");
+    String wf_pass = server.arg("pass");
+    if(wf_name.length() != 0 && wf_pass.length() >= 8 && (wf_name != WIFI_SSID || wf_pass != WIFI_PASSWORD)){
+      WIFI_SSID = wf_name;
+      WIFI_PASSWORD = wf_pass;
+      Serial.println("WifiInfo:");
+      Serial.println(WIFI_SSID);
+      Serial.println(WIFI_PASSWORD);
+      writeWifiInfoToEEPROM(WIFI_SSID, WIFI_PASSWORD);
+    }
+  }
+  String html_content = "";
+  html_content += "<html>";
+  html_content += "<head>";
+  html_content += "<meta charset='UTF-8'>";
+  html_content += "<h2>Enter wifi information to connect the device</h2>";
+  html_content += "</head>";
+  html_content += "<body>";
+  html_content += "<form method='post'>WiFiName: <input type='text' name='name' value='" + WIFI_SSID + "' pattern='.{1,}' required title='1 characters minimum'/> <br />WiFiPass: <input type='text' name='pass' placeholder='Minimum of 8 characters' value='" + WIFI_PASSWORD + "' pattern='.{8,}' required title='8 characters minimum'/> <br /><input class='button' type='submit' value='Save'/></form>";
+  html_content += "</body>";
+  html_content += "</html>";
+  server.send(200, "text/html", html_content);
+}
+
 void setup() {
   pinMode(IRLEDPIN, OUTPUT);
   pinMode(WFLED, OUTPUT);
   pinMode(RXPINIR, INPUT);
   pinMode(LEDPIN, OUTPUT);
+  pinMode(FLASHPIN, INPUT_PULLUP);
   digitalWrite(LEDPIN, LOW);
   digitalWrite(IRLEDPIN, LOW);
   digitalWrite(WFLED, HIGH);
+  
+  
+  int wf_led = HIGH;
   Serial.begin(9600);
-  WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
-  while (WiFi.status() != WL_CONNECTED) {
-    delay(500);
-  }
-  timeClient.begin();
+  //
+  hasWifiInfo = lockupWifiInfo();
+  if(hasWifiInfo){
+    Serial.println("WifiInfo:");
+    Serial.println(WIFI_SSID);
+    Serial.println(WIFI_PASSWORD);
+    WiFi.mode(WIFI_STA);
+    WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
+    attachInterrupt(digitalPinToInterrupt(FLASHPIN), flash_Interrupt_Handler, FALLING);
+    while (WiFi.status() != WL_CONNECTED) {
+      delay(500);
+      wf_led = !wf_led;
+      digitalWrite(WFLED, wf_led);
+      if(flashPressed) clearEEPROM();
+    }
+    Serial.println("Wifi Connected!");
 
-  for (int i = 0; i < 10; i++) schedule[i].pos = i;
-  digitalWrite(WFLED, LOW);
-  Firebase.begin(FIREBASE_HOST, FIREBASE_AUTH);
-  Firebase.stream("/device");
-  delay(500);
-  timeClient.update();
-  Firebase.setString("device/cmd", "RESET");
-   unsigned long _ct = timeClient.getEpochTime();
-   for(int i = 0; i < 10; i++){
-     String _s = Firebase.getString("device/schedule/s" + String(i));
-     int f = Firebase.getInt("device/sduration/s" + String(i));
-     int lp = Firebase.getInt("device/sloop/s" + String(i));
-     String ss = Firebase.getString("device/sstatus/s" + String(i));
-     if(ss == "DONE" || ss == "" || ss == "OK" || ss == "CANCEL"){
-      schedule[i].stop();
-      continue;
-     }     
-     if(f < _ct){
-      Firebase.setString("device/sstatus/s" + String(i), "TIME_ERROR");
-      continue; 
-     }
-     if(!validateIRcode(_s)){
-      Firebase.setString("device/sstatus/s" + String(i), "INVALID_IRCODE");
-      continue;
-     }
-     if(lp < 0 || lp > 24){
-      Firebase.setString("device/sstatus/s" + String(i), "INVALID_LOOP");
-      continue;
-     }
-     schedule[i].data = _s;
-     schedule[i].duration = (unsigned long)f - _ct;
-     schedule[i].set();
-     Firebase.setString("device/sstatus/s" + String(i), "WAIT");
-   }
+    timeClient.begin();
+    for (int i = 0; i < 10; i++) schedule[i].pos = i;
+    digitalWrite(WFLED, HIGH);
+    Firebase.begin(FIREBASE_HOST, FIREBASE_AUTH);
+    Firebase.stream("/device");
+    delay(500);
+    timeClient.update();
+    Firebase.setString("device/cmd", "RESET");
+    unsigned long _ct = timeClient.getEpochTime();
+    for(int i = 0; i < 10; i++){
+      String _s = Firebase.getString("device/schedule/s" + String(i));
+      int f = Firebase.getInt("device/sduration/s" + String(i));
+      int lp = Firebase.getInt("device/sloop/s" + String(i));
+      String ss = Firebase.getString("device/sstatus/s" + String(i));
+      if(ss == "DONE" || ss == "" || ss == "OK" || ss == "CANCEL"){
+       schedule[i].stop();
+       continue;
+      }     
+      if(f < _ct){
+       Firebase.setString("device/sstatus/s" + String(i), "TIME_ERROR");
+       continue; 
+      }
+      if(!validateIRcode(_s)){
+       Firebase.setString("device/sstatus/s" + String(i), "INVALID_IRCODE");
+       continue;
+      }
+      if(lp < 0 || lp > 24){
+       Firebase.setString("device/sstatus/s" + String(i), "INVALID_LOOP");
+       continue;
+      }
+      schedule[i].data = _s;
+      schedule[i].duration = (unsigned long)f - _ct;
+      schedule[i].set();
+      Firebase.setString("device/sstatus/s" + String(i), "WAIT");
+    }
+    digitalWrite(WFLED, LOW);
+    flashPressed = false;
+  } else{
+    Serial.println("There is no WifiInfo!");
+    Serial.print("Configuring access point...");
+    WiFi.mode(WIFI_AP);
+    WiFi.softAP("ESP8266", "12345678");
+    delay(100);
+    IPAddress ip(192, 168, 1, 1);
+    IPAddress n_mask(255, 255, 255, 0);
+    WiFi.softAPConfig(ip, ip, n_mask);
+    IPAddress myIP = WiFi.softAPIP();
+    Serial.print("AP IP address: ");
+    Serial.println(myIP);
+    server.on("/", handleWSRoot);
+    server.begin();
+    Serial.println("HTTP server started");
+    flashPressed = false;
+    attachInterrupt(digitalPinToInterrupt(FLASHPIN), flash_Interrupt_Handler, FALLING);
+  }
+  //
 }
 
-void loop() {
-//  delay(5000);
-//  sendIR();
+void mainOp(){
   timeClient.update();
   for(int i = 0;i < 10; i++){
     if(schedule[i].st) schedule[i].next();
@@ -399,4 +517,33 @@ void loop() {
         }
      }
   }   
+}
+
+void clearEEPROM(){
+  Serial.println("Flash button pressed!");
+  digitalWrite(LEDPIN, HIGH);
+  EEPROM.begin(512);
+  int w_a = EEPROM.read(0);
+  delay(200);
+  if(w_a){
+    EEPROM.write(0, 0);
+    delay(5);
+    Serial.println("Cleared WifiInfo");
+  }
+  EEPROM.end();
+  WIFI_SSID = "";
+  WIFI_PASSWORD = "";
+  flashPressed = false;
+  digitalWrite(LEDPIN, LOW);
+}
+
+void loop() {
+  if(flashPressed){
+    clearEEPROM();
+  }
+  if(hasWifiInfo){
+    mainOp();
+  } else{
+    server.handleClient();
+  }
 }
